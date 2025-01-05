92836f4b7dd4d1faae03742d9550e52e4b43a290
Korey Peters <me@koreypeters.org>
2025-01-05 15:38:24 -0700

    Added links and home pages to Polium game

--- polium/urls.py
+++ polium/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
 from . import views
- 
+
 urlpatterns = [
-   path("candidates/", views.candidates_list, name="polium_candidates_list"),
-   path("candidates/<str:sqid>", views.candidates_detail, name="polium_candidates_detail"),
-   path("elections/", views.elections_list, name="polium_elections_list"),
-   path("elections/<str:sqid>", views.elections_detail, name="polium_elections_detail"),
-   path("politicians/", views.politicians_list, name="polium_politicians_list"),
-   path("politicians/<str:sqid>", views.politicians_detail, name="polium_politicians_detail"),
+    path("", views.polium_root, name="polium"),
+    path("candidates/", views.candidates_list, name="polium_candidates_list"),
+    path(
+        "candidates/<str:sqid>",
+        views.candidates_detail,
+        name="polium_candidates_detail",
+    ),
+    path("elections/", views.elections_list, name="polium_elections_list"),
+    path(
+        "elections/<str:sqid>", views.elections_detail, name="polium_elections_detail"
+    ),
+    path("politicians/", views.politicians_list, name="polium_politicians_list"),
+    path(
+        "politicians/<str:sqid>",
+        views.politicians_detail,
+        name="polium_politicians_detail",
+    ),
 ]
--- polium/views.py
+++ polium/views.py
@@ -3,25 +3,34 @@ from django.shortcuts import get_object_or_404, render
 from polium.models import Candidate, Election, Politician
 
 
+def polium_root(request):
+    return render(request, "polium/root.html", locals())
+
+
 def candidates_list(request):
     candidates = Candidate.objects.all()
     return render(request, "polium/candidates_list.html", locals())
+
 
 def candidates_detail(request, sqid):
     candidate = get_object_or_404(Candidate, sqid=sqid)
     return render(request, "polium/candidates_detail.html", locals())
 
+
 def elections_list(request):
     elections = Election.objects.all()
     return render(request, "polium/elections_list.html", locals())
+
 
 def elections_detail(request, sqid):
     election = get_object_or_404(Election, sqid=sqid)
     return render(request, "polium/elections_detail.html", locals())
 
+
 def politicians_list(request):
     politicians = Politician.objects.all()
     return render(request, "polium/politicians_list.html", locals())
+
 
 def politicians_detail(request, sqid):
     politician = get_object_or_404(Politician, sqid=sqid)
